b7db3ec34b1312d73a41e3b19fc62658dadb33d4
viniciussardi <viniciussardinhatec@gmail.com>
2024-06-27 15:27:38 -0300

    commit

---
+++ provapratica14.py
@@ -0,0 +1,8 @@
+print("bem vindo ao velocimetro 5.0")
+
+distancia = float(input("digite a distancia percorrida: "))
+tempo = float(input("digite o tempo em minutos gasto para percorrer a distancia informada a cima: "))
+
+velMedia = distancia / tempo
+
+print(f"sua velocidade media é {velMedia}km/h")
