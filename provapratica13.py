fbc19559da1496f3c47666640a90f8a3a16e93e8
viniciussardi <viniciussardinhatec@gmail.com>
2024-06-27 14:44:18 -0300

    commit

---
+++ provapratica13.py
@@ -0,0 +1,6 @@
+salBruto = int(input("digite seu salario bruto"))
+descSal = int(input("agora digite o desconto do seu salario"))
+
+SalLiquido = salBruto - descSal
+
+print(f"seu salario liquido é {SalLiquido}")
